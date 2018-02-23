815db9bc5ce2a3173a3feea5f4395cd3ca2d3f07
Marat <unk13b16@yandex.ru>
2018-02-23 13:26:08 +0400

    homework check result

--- app/src/main/java/com/example/ivan/hunterworld/MainActivity.java
+++ app/src/main/java/com/example/ivan/hunterworld/MainActivity.java
@@ -9,10 +9,11 @@ import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    // лучше давать классам осознанные имена. AuthorizationActivity например
 
     private static final int RC_SIGN_IN = 123;
 
@@ -20,24 +21,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
+        List<AuthUI.IdpConfig> providers = Collections.singletonList(new AuthUI.IdpConfig.EmailBuilder().build());
+        // Если нужно поместить лишь один элемент в лист, то лучше использовать singletonList
         startActivityForResult(AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers).build()
-                ,RC_SIGN_IN);
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build()
+                , RC_SIGN_IN);
+        // желательно форматировать код в соответствии и дефолтным code style (ctrl+alt+L hotkey)
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RC_SIGN_IN){
+        if (requestCode == RC_SIGN_IN) {
+            // баг - проверялся не тот код, потому код в блоке не выполнялся
             FirebaseUser response = FirebaseAuth.getInstance().getCurrentUser();
-
-            if(requestCode == RESULT_OK){
+// а это действительно нужно, если результат никак не используется?
+            if (resultCode == RESULT_OK) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                if(user != null){
+                if (user != null) {
                     Toast.makeText(this, user.toString(), Toast.LENGTH_SHORT).show();
-                }else {
-
+                    // показ тоста лучше вынести в отдельную функцию, т.к. еще возможно пригодится
+                } else {
+// тут лучше либо определить какое-то действие, либо исключить else блок
                 }
             }
         }
